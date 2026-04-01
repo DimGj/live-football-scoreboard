7bd099c46b6a23f6134637fd8515916835020bc3
DimGj <dgjergji.2000@yahoo.com>
2026-04-01 20:32:41 +0300

    flatten project structure to use repo root as Maven project

--- src/test/java/com/dimitris/scoreboard/ScoreboardTest.java
+++ src/test/java/com/dimitris/scoreboard/ScoreboardTest.java
@@ -1,9 +1,7 @@
 package com.dimitris.scoreboard;
 
 import org.junit.jupiter.api.Test;
-
 import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ScoreboardTest {
